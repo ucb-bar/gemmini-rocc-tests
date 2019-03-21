e71befc2a99762d035ebd164d98c63fdff3636e8
Hasan Genc <hngenc@berkeley.edu>
2019-03-20 19:10:35 -0700

    Added matmul testing utility functions

--- bareMetalC/matmul_os.c
+++ bareMetalC/matmul_os.c
@@ -8,91 +8,51 @@
 #include "include/systolic.h"
 #include "util.h"
 
-void matmul(elem_t A[DIM][DIM], elem_t B[DIM][DIM], elem_t D[DIM][DIM], elem_t C[DIM][DIM]) {
-    for (size_t r = 0; r < DIM; r++)
-        for (size_t c = 0; c < DIM; c++) {
-            C[r][c] = D[r][c];
-            for (size_t k = 0; k < DIM; k++)
-                C[r][c] += A[r][k]*B[k][c];
-        }
-}
-
-void transpose(elem_t in[DIM][DIM], elem_t out[DIM][DIM]) {
-    for (size_t r = 0; r < DIM; r++)
-        for (size_t c = 0; c < DIM; c++)
-            out[c][r] = in[r][c];
-}
-
-void printMatrix(elem_t m[DIM][DIM]) {
-  for (size_t i = 0; i < DIM; ++i) {
-    for (size_t j = 0; j < DIM; ++j)
-      printf("%d ", m[i][j]);
-    printf("\n");
-  }
-}
-
 int main() {
-  static elem_t A[DIM][DIM];
-  static elem_t B[DIM][DIM];
-  static elem_t C[DIM][DIM];
-  static elem_t D[DIM][DIM];
+  for (int n = 0; n < 2; n++) {
+    static matmul_t mm;
 
-  static elem_t gold[DIM][DIM];
-
-  static elem_t A_tp[DIM][DIM];
-
-  for (size_t i = 0; i < DIM; ++i) {
-    for (size_t j = 0; j < DIM; ++j) {
-      // A = incrementing values row by row
-      A[i][j] = i*DIM + j;
-      // B = identity matrix
-      B[i][j] = (i == j) ? 1 : 0;
-      // D = zeros, TODO try to use matmul.preload(rd = 1) instead
-      D[i][j] = 0;
-    }
-  }
-
-  matmul(A, B, D, gold);
-  transpose(A, A_tp);
-
-#ifdef DEBUG_PRINTS
-  printMatrix(A);
-  printMatrix(B);
-#endif
-  printf("Moving in\n");
-
-  for (size_t i = 0; i < DIM; ++i) {
-    matmul_mvin(A_tp[i], i);
-    matmul_mvin(B[i], DIM + i);
-    matmul_mvin(D[i], 2*DIM + i);
-  }
-
-  printf("Setting mode\n");
-  matmul_setmode(0);
-  printf("Preloading\n");
-  matmul_preload_no_rd(2*DIM, 3*DIM);
-  printf("Computing\n");
-  matmul_compute_preloaded(0x0, DIM);
-
-  printf("Moving out\n");
-  for (size_t i = 0; i < DIM; ++i) {
-    matmul_mvout(C[i], 3*DIM + i);
-  }
-
-  printf("Gold:\n");
-  printMatrix(gold);
-  printf("Actual:\n");
-  printMatrix(C);
-
-  for (size_t i = 0; i < DIM; ++i) {
-    for (size_t j = 0; j < DIM; ++j) {
-      if (C[i][j] != gold[i][j]) {
-        printf("C[%lu][%lu] = %d\n", i, j, C[i][j]);
-        printf("Gold[%lu][%lu] = %d\n", i, j, gold[i][j]);
-        exit(1);
+    for (size_t i = 0; i < DIM; ++i) {
+      for (size_t j = 0; j < DIM; ++j) {
+        // A = incrementing values row by row
+        mm.A[i][j] = i*DIM + j;
+        // B = identity matrix
+        mm.B[i][j] = (i == j) ? 1 : 0;
+        // D = zeros, TODO try to use matmul.preload(rd = 1) instead
+        mm.D[i][j] = 0;
       }
     }
+
+    init_matmul(&mm);
+
+    printf("Moving in\n");
+    for (size_t i = 0; i < DIM; ++i) {
+      matmul_mvin(mm.A_tp[i], i);
+      matmul_mvin(mm.B[i], DIM + i);
+      matmul_mvin(mm.D[i], 2*DIM + i);
+    }
+
+    printf("Setting mode\n");
+    matmul_setmode(0);
+    printf("Preloading\n");
+    matmul_preload_no_rd(2*DIM, 3*DIM);
+    printf("Computing\n");
+    matmul_compute_preloaded(0x0, DIM);
+
+    printf("Moving out\n");
+    for (size_t i = 0; i < DIM; ++i) {
+      matmul_mvout(mm.C[i], 3*DIM + i);
+    }
+
+    printf("Gold:\n");
+    printMatrix(mm.gold);
+    printf("Actual:\n");
+    printMatrix(mm.C);
+
+    if (!matmul_is_correct(&mm))
+        exit(1);
   }
+
   exit(0);
 }
 
--- include/systolic.h
+++ include/systolic.h
@@ -5,11 +5,63 @@
 
 // Dimension of the systolic array
 // Should be tileColumns*meshColumns
-#define DIM 16
+#define DIM 4
 // Datatype of the systolic array
 // TODO: Should be signed, but then need to add a bias term below
-typedef uint8_t elem_t;
+typedef uint16_t elem_t;
 
+// Matmul utility functions
+typedef struct matmul_t {
+    elem_t A[DIM][DIM];
+    elem_t B[DIM][DIM];
+    elem_t C[DIM][DIM];
+    elem_t D[DIM][DIM];
+
+    elem_t A_tp[DIM][DIM];
+    elem_t B_tp[DIM][DIM];
+
+    elem_t gold[DIM][DIM];
+} matmul_t;
+
+void matmul(elem_t A[DIM][DIM], elem_t B[DIM][DIM], elem_t D[DIM][DIM], elem_t C[DIM][DIM]) {
+  for (size_t r = 0; r < DIM; r++)
+    for (size_t c = 0; c < DIM; c++) {
+      C[r][c] = D[r][c];
+      for (size_t k = 0; k < DIM; k++)
+        C[r][c] += A[r][k]*B[k][c];
+    }
+}
+
+void transpose(elem_t in[DIM][DIM], elem_t out[DIM][DIM]) {
+  for (size_t r = 0; r < DIM; r++)
+    for (size_t c = 0; c < DIM; c++)
+      out[c][r] = in[r][c];
+}
+
+void printMatrix(elem_t m[DIM][DIM]) {
+  for (size_t i = 0; i < DIM; ++i) {
+    for (size_t j = 0; j < DIM; ++j)
+      printf("%d ", m[i][j]);
+    printf("\n");
+  }
+}
+
+int matmul_is_correct(matmul_t * mm) {
+  for (size_t i = 0; i < DIM; ++i)
+    for (size_t j = 0; j < DIM; ++j)
+      if (mm->C[i][j] != mm->gold[i][j])
+          return 0;
+  return 1;
+}
+
+// Run this function after setting the values of A, B, and D
+void init_matmul(matmul_t * mm) {
+  matmul(mm->A, mm->B, mm->D, mm->gold);
+  transpose(mm->A, mm->A_tp);
+  transpose(mm->B, mm->B_tp);
+}
+
+// Accelerator interface
 #include "rocc-software/src/xcustom.h"
 
 #define k_MVIN 2
