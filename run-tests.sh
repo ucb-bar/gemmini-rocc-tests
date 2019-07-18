3dfb57ce7289f16cc4d3bf920ce2789f394d0c3f
alonamid <alonamid@eecs.berkeley.edu>
2019-07-18 16:50:14 +0000

    fix script

--- run-tests.sh
+++ run-tests.sh
@@ -2,11 +2,11 @@
 
 echo "*****************TEST RESULTS*************" > test_output.txt
 echo "=========tiled_matmul_os-linux=========" >> test_output.txt
-root/systolic-rocc-tests/build/bareMetalC/tiled_matmul_os-linux >> test_output.txt
+/root/systolic-rocc-tests/build/bareMetalC/tiled_matmul_os-linux >> test_output.txt
 echo "=========tiled_matmul_ws-linux=========" >> test_output.txt
-root/systolic-rocc-tests/build/bareMetalC/tiled_matmul_ws-linux >> test_output.txt
+/root/systolic-rocc-tests/build/bareMetalC/tiled_matmul_ws-linux >> test_output.txt
 echo "========mobilenet=========" >> test_output.txt
-root/systolic-rocc-tests/build/mobilenet/test >> test_output.txt
+/root/systolic-rocc-tests/build/mobilenet/test >> test_output.txt
 echo "========mobilenet=========" >> test_output.txt
-root/systolic-rocc-tests/build/mobilenet/test cpu >> test_output.txt
+/root/systolic-rocc-tests/build/mobilenet/test cpu >> test_output.txt
 cat test_output.txt
