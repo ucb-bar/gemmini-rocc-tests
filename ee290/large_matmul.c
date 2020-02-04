5cc60f497f496faf6c10721b164be16f112cb335
Hasan Genc <hngenc@berkeley.edu>
2020-02-04 00:14:12 -0800

    Renamed tests

--- ee290/large_matmul.c
+++ ee290/large_matmul.c
@@ -12,34 +12,9 @@
 
 #define CHECK_RESULT 1
 
-#define NO_BIAS 1
-#define FULL_BIAS_WIDTH 0
-
-#if FULL_BIAS_WIDTH
-typedef acc_t ACC_T;
-#else
-typedef elem_t ACC_T;
-#endif
-
-#ifndef BAREMETAL
-#define MAT_DIM_I 512
-#define MAT_DIM_K 512
-#define MAT_DIM_J 512
-#define TILE_I_FACTOR 1
-#define TILE_J_FACTOR 1
-#define TILE_K_FACTOR 1
-#else
 #define MAT_DIM_I 64
 #define MAT_DIM_K 64
 #define MAT_DIM_J 64
-#define TILE_I_FACTOR 2
-#define TILE_J_FACTOR 2
-#define TILE_K_FACTOR 2
-#endif
-
-#if ((MAT_DIM_I % (TILE_I_FACTOR*DIM)) != 0) || ((MAT_DIM_J % (TILE_J_FACTOR*DIM)) != 0) || ((MAT_DIM_K % (TILE_K_FACTOR*DIM)) != 0)
-#error Matrix dimensions are not divisble by tiling factors
-#endif
 
 void print_tile(elem_t* in, int tile_dim) {
   for (size_t r = 0; r < tile_dim; r++) {
@@ -51,10 +26,10 @@ void print_tile(elem_t* in, int tile_dim) {
   }
 }
 
-void full_matmul(elem_t A[MAT_DIM_I][MAT_DIM_K], elem_t B[MAT_DIM_K][MAT_DIM_J], ACC_T D[MAT_DIM_I][MAT_DIM_J], int64_t C_full[MAT_DIM_I][MAT_DIM_J]) {
+void full_matmul(elem_t A[MAT_DIM_I][MAT_DIM_K], elem_t B[MAT_DIM_K][MAT_DIM_J], int64_t C_full[MAT_DIM_I][MAT_DIM_J]) {
   for (size_t r = 0; r < MAT_DIM_I; r++)
     for (size_t c = 0; c < MAT_DIM_J; c++) {
-      C_full[r][c] = D[r][c];
+      C_full[r][c] = 0;
       for (size_t k = 0; k < MAT_DIM_K; k++)
         C_full[r][c] += A[r][k]*B[k][c];
     }
@@ -101,7 +76,6 @@ int main() {
     static elem_t full_A[MAT_DIM_I][MAT_DIM_K] row_align(1);
     static elem_t full_B[MAT_DIM_K][MAT_DIM_J] row_align(1);
     static elem_t full_C[MAT_DIM_I][MAT_DIM_J] row_align(1);
-    static ACC_T full_D[MAT_DIM_I][MAT_DIM_J] row_align_acc(1);
 
     static int64_t gold_full[MAT_DIM_I][MAT_DIM_J];
     static elem_t gold[MAT_DIM_I][MAT_DIM_J];
@@ -120,21 +94,14 @@ int main() {
         full_B[i][j] = rand() % 2;
       }
     }
-
-    // printf("Init D\n");
-    for (size_t i = 0; i < MAT_DIM_I; ++i) {
-      for (size_t j = 0; j < MAT_DIM_J; ++j) {
-        full_D[i][j] = NO_BIAS ? 0 : rand() % 2;
-      }
-    }
 #endif
 
 #if CHECK_RESULT == 1
     printf("Starting slow CPU matmul\n");
     unsigned long cpu_start = read_cycles();
-    full_matmul(full_A, full_B, full_D, gold_full);
+    full_matmul(full_A, full_B, gold_full);
     unsigned long cpu_end = read_cycles();
-    printf("Cycles taken: %u\n", cpu_end-cpu_start);
+    printf("Cycles taken by CPU: %u\n", cpu_end-cpu_start);
     full_matshift(gold_full, gold, 0);
 #endif
 
@@ -142,12 +109,12 @@ int main() {
     unsigned long start = read_cycles();
 
     tiled_matmul_option(MAT_DIM_I, MAT_DIM_J, MAT_DIM_K,
-            full_A, full_B, NO_BIAS ? NULL : full_D, full_C,
-            NO_ACTIVATION, 0, 0, FULL_BIAS_WIDTH,
+            full_A, full_B, NULL, full_C,
+            NO_ACTIVATION, 0, 0, 0,
             WS);
 
     unsigned long end = read_cycles();
-    printf("Cycles taken: %u\n", end-start);
+    printf("Cycles taken by Gemmini: %u\n", end-start);
 
 #if CHECK_RESULT == 1
     if (!full_is_equal(full_C, gold)) {
