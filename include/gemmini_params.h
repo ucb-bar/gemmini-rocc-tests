148f0a6225af8a13549bed7625ab88b43acf5d50
Sam Steffl <sam@ssteffl.com>
2020-08-25 02:21:31 -0700

    fixed params?

--- include/gemmini_params.h
+++ include/gemmini_params.h
@@ -14,18 +14,18 @@
 #define MAX_BLOCK_LEN_ACC (MAX_BYTES/(DIM*4))
 
 typedef int8_t elem_t;
-elem_t elem_t_max = 127;
-elem_t elem_t_min = -128;
+static const elem_t elem_t_max = 127;
+static const elem_t elem_t_min = -128;
 typedef int32_t acc_t;
 typedef int64_t full_t;
 
 #define HAS_MVIN_SCALE
-typedef int32_t scale_t;
-typedef uint32_t scale_t_bits;
+typedef int8_t scale_t;
+typedef uint8_t scale_t_bits;
 
 #define HAS_MVIN_ACC_SCALE
-typedef int32_t scale_acc_t;
-typedef uint32_t scale_acc_t_bits;
+typedef int8_t scale_acc_t;
+typedef uint8_t scale_acc_t_bits;
 
 #define row_align(blocks) __attribute__((aligned(blocks*DIM*sizeof(elem_t))))
 #define row_align_acc(blocks) __attribute__((aligned(blocks*DIM*sizeof(acc_t))))
