820799ae2e911595aa0cf2b7231236cfa7aabdde
Hasan Genc <hngenc@berkeley.edu>
2020-11-28 17:04:54 -0800

    Added tranpose tests

--- bareMetalC/tiled_matmul_ws.c
+++ bareMetalC/tiled_matmul_ws.c
@@ -139,6 +139,7 @@ int main() {
             MAT_DIM_K, MAT_DIM_J, MAT_DIM_J, MAT_DIM_J,
             MVIN_SCALE_IDENTITY, MVIN_SCALE_IDENTITY, MVIN_SCALE_IDENTITY,
             NO_ACTIVATION, ACC_SCALE_IDENTITY, 0, false,
+            false, false,
             WS);
 
     unsigned long end = read_cycles();
