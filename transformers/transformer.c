1b1c77e716cbec96bf68ec7d0ca62d396f3d161e
Hasan Genc <hngenc@berkeley.edu>
2023-06-25 21:52:51 -0400

    Standalone layernorm (#40)
    Adds a standalone layernorm function, which performs a layernorm that's NOT overlapped with a matmul

--- transformers/transformer.c
+++ transformers/transformer.c
@@ -16,11 +16,19 @@ void attention(int hidden_dim, int expansion_dim, int num_heads, int seq_len,
         elem_t * out, elem_t * resadd_out,
         const elem_t * Wq, const elem_t * Wk, const elem_t * Wv, const elem_t * Wo,
 
+        const acc_t * Wq_b, const acc_t * Wk_b, const acc_t * Wv_b,
+        const acc_t * Wo_b,
+
         elem_t * Q_buf, elem_t * K_buf, elem_t * V_buf,
-        elem_t * attn_buf, elem_t * out_buf)
+        elem_t * attn_buf, elem_t * out_buf, acc_t * out_buf_acc)
 {
-    const int hidden_dim_compressed = hidden_dim / compression_factor;
-    const int hidden_dim_per_head = hidden_dim_compressed / num_heads;
+    int hidden_dim_compressed = hidden_dim / compression_factor;
+    int hidden_dim_per_head = hidden_dim_compressed / num_heads;
+
+    if (compression_factor < 0) {
+        hidden_dim_compressed = hidden_dim;
+        hidden_dim_per_head = (hidden_dim_compressed / 12) * (-compression_factor);
+    }
 
     // Q = Wq * input
     // K = Wk * enc_out
@@ -29,15 +37,17 @@ void attention(int hidden_dim, int expansion_dim, int num_heads, int seq_len,
     for (int i = 0; i < qkv_matmuls_n; i++) {
         const elem_t * qkv_weights[] = {Wq, Wk, Wv};
         const elem_t * qkv_ins[] = {input, enc_out, enc_out};
+        const acc_t * qkv_bs[] = {Wq_b, Wk_b, Wk_b};
         elem_t * qkv_outs[] = {Q_buf, K_buf, V_buf};
 
         const elem_t * qkv_w = qkv_weights[i];
         const elem_t * qkv_in = qkv_ins[i];
+        const acc_t * qkv_b = qkv_bs[i];
         elem_t * qkv_out = qkv_outs[i];
 
         tiled_matmul_auto(seq_len, hidden_dim_compressed, hidden_dim,
             /*A=*/ qkv_in, /*B=*/ qkv_w,
-            /*D=*/ NULL, /*C=*/ qkv_out,
+            /*D=*/ qkv_b, /*C=*/ qkv_out,
             /*stride_A=*/hidden_dim, /*stride_B=*/hidden_dim, /*stride_D=*/0, /*stride_C=*/hidden_dim,
             MVIN_SCALE_IDENTITY, MVIN_SCALE_IDENTITY, MVIN_SCALE_IDENTITY,
             NO_ACTIVATION, /*scale=*/ ACC_SCALE_IDENTITY, /*bert_scale=*/ 0,
@@ -62,7 +72,7 @@ void attention(int hidden_dim, int expansion_dim, int num_heads, int seq_len,
             /*D=*/ NULL, /*C=*/ C,
             /*stride_A=*/hidden_dim, /*stride_B=*/hidden_dim, /*stride_D=*/0, /*stride_C=*/seq_len,
             MVIN_SCALE_IDENTITY, MVIN_SCALE_IDENTITY, MVIN_SCALE_IDENTITY,
-            /*SOFTMAX*/ LAYERNORM, /*scale=*/ ACC_SCALE_IDENTITY, /*bert_scale=*/ 0,
+            SOFTMAX, /*scale=*/ ACC_SCALE_IDENTITY, /*bert_scale=*/ 0,
             /*repeating_bias=*/ false,
             false, /*transpose_B=*/ true,
             false, false,
@@ -93,21 +103,26 @@ void attention(int hidden_dim, int expansion_dim, int num_heads, int seq_len,
 
     gemmini_fence();
 
-    // out = out_buf * Wo
-    // out = LN(out)
+    // out_buf_acc = out_buf * Wo
     tiled_matmul_auto(seq_len, hidden_dim, hidden_dim_compressed,
         /*A=*/ out_buf, /*B=*/ Wo,
-        /*D=*/ NULL, /*C=*/ out,
+        /*D=*/ Wo_b, /*C=*/ out_buf_acc,
         /*stride_A=*/hidden_dim, /*stride_B=*/hidden_dim, /*stride_D=*/0, /*stride_C=*/hidden_dim,
         MVIN_SCALE_IDENTITY, MVIN_SCALE_IDENTITY, MVIN_SCALE_IDENTITY,
-        LAYERNORM, /*scale=*/ ACC_SCALE_IDENTITY, /*bert_scale=*/ 0,
+        NO_ACTIVATION, /*scale=*/ ACC_SCALE_IDENTITY, /*bert_scale=*/ 0,
         /*repeating_bias=*/ false,
         false, /*transpose_B=*/ false,
-        false, false,
+        true, false,
         0,
         WS);
 
     gemmini_fence();
+
+    // out = LN(out_buf_acc)
+    tiled_norm_auto(seq_len, hidden_dim,
+        (acc_t*)out_buf_acc, (elem_t*)out,
+        ACC_SCALE_IDENTITY,
+        LAYERNORM, WS);
 
     // input = out + input
     tiled_resadd_auto(seq_len, hidden_dim,
@@ -128,7 +143,7 @@ void ffn(int hidden_dim, int expansion_dim, int seq_len,
         const elem_t * ff1_w, const elem_t * ff2_w,
         const acc_t * ff1_b, const acc_t * ff2_b,
 
-        elem_t * out_buf)
+        elem_t * out_buf, acc_t * out_buf_acc)
 {
     // out = FF1(input)
     // out = GELU(out)
@@ -146,20 +161,26 @@ void ffn(int hidden_dim, int expansion_dim, int seq_len,
 
     gemmini_fence();
 
-    // out = FF2(out)
-    // out = LN(out)
-
+    // out_buf_acc = FF2(out)
     tiled_matmul_auto(seq_len, hidden_dim, expansion_dim, 
         /*A=*/ out_buf, /*B=*/ ff2_w,
-        /*D=*/ ff2_b, /*C=*/ out,
+        /*D=*/ ff2_b, /*C=*/ out_buf_acc,
         /*stride_A=*/expansion_dim, /*stride_B=*/hidden_dim, /*stride_D=*/expansion_dim, /*stride_C=*/expansion_dim,
         MVIN_SCALE_IDENTITY, MVIN_SCALE_IDENTITY, MVIN_SCALE_IDENTITY,
-        LAYERNORM, /*scale=*/ ACC_SCALE_IDENTITY, /*bert_scale=*/ 0,
+        NO_ACTIVATION, /*scale=*/ ACC_SCALE_IDENTITY, /*bert_scale=*/ 0,
         /*repeating_bias=*/ true,
         false, /*transpose_B=*/ false,
-        false, false,
+        true, false,
         0,
         WS);
+
+    gemmini_fence();
+
+    // out = LN(out_buf_acc)
+    tiled_norm_auto(seq_len, hidden_dim,
+        (acc_t*)out_buf_acc, (elem_t*)out,
+        ACC_SCALE_IDENTITY,
+        LAYERNORM, WS);
 
     gemmini_fence();
 
@@ -187,11 +208,17 @@ uint64_t encoder_decoder(
         const elem_t * input, const elem_t * enc_out, elem_t * out,
         const elem_t * Wq, const elem_t * Wk, const elem_t * Wv, const elem_t * Wo,
         const elem_t * Wq_cross, const elem_t * Wk_cross, const elem_t * Wv_cross, const elem_t * Wo_cross,
+
+        const acc_t * Wq_b, const acc_t * Wk_b, const acc_t * Wv_b,
+        const acc_t * Wo_b,
+        const acc_t * Wq_cross_b, const acc_t * Wk_cross_b, const acc_t * Wv_cross_b,
+        const acc_t * Wo_cross_b,
+
         const elem_t * ff1_w, const elem_t * ff2_w,
         const acc_t * ff1_b, const acc_t * ff2_b,
 
         elem_t * Q_buf, elem_t * K_buf, elem_t * V_buf,
-        elem_t * attn_buf, elem_t * out_buf,
+        elem_t * attn_buf, elem_t * out_buf, acc_t * out_buf_acc,
         elem_t * resadd1_buf, elem_t * resadd2_buf)
 {
     const bool is_encoder = enc_out == NULL;
@@ -202,16 +229,24 @@ uint64_t encoder_decoder(
         input, input,
         out, resadd1_buf,
         Wq, Wk, Wv, Wo,
+
+        Wq_b, Wk_b, Wv_b,
+        Wo_b,
+
         Q_buf, K_buf, V_buf,
-        attn_buf, out_buf);
+        attn_buf, out_buf, out_buf_acc);
 
     if (!is_encoder) {
         attention(hidden_dim, expansion_dim, cross_num_heads, seq_len, compression_factor,
             resadd1_buf, enc_out,
             out, resadd2_buf,
             Wq_cross, Wk_cross, Wv_cross, Wo_cross,
+
+            Wq_cross_b, Wk_cross_b, Wv_cross_b,
+            Wo_cross_b,
+
             Q_buf, K_buf, V_buf,
-            attn_buf, out_buf);
+            attn_buf, out_buf, out_buf_acc);
     }
 
     ffn(hidden_dim, expansion_dim, seq_len,
@@ -219,7 +254,7 @@ uint64_t encoder_decoder(
         out,
         ff1_w, ff2_w,
         ff1_b, ff2_b,
-        out_buf);
+        out_buf, out_buf_acc);
 
     uint64_t end = read_cycles();
 
@@ -227,8 +262,11 @@ uint64_t encoder_decoder(
 }
 
 #define ENCODER_DECODER(hidden_dim, expansion_dim, num_heads, cross_num_heads, seq_len, compression_factor, input, enc_out, output) ({ \
+    \
     static const elem_t Wqkvo[4][hidden_dim][hidden_dim]; \
     static const elem_t Wqkvo_cross[4][hidden_dim][hidden_dim]; \
+    static const acc_t Wqkvo_b[4][hidden_dim]; \
+    static const acc_t Wqkvo_cross_b[4][hidden_dim]; \
     static const elem_t ff_w[2][hidden_dim*expansion_dim]; \
     static const acc_t ff1_b[expansion_dim]; \
     static const acc_t ff2_b[hidden_dim]; \
@@ -236,6 +274,7 @@ uint64_t encoder_decoder(
     static elem_t QKV_buf[3][seq_len][hidden_dim];\
     static elem_t attn_buf[num_heads][seq_len][seq_len];\
     static elem_t out_buf[seq_len][expansion_dim];\
+    static acc_t out_buf_acc[seq_len][hidden_dim];\
     static elem_t resadd1_buf[seq_len][hidden_dim];\
     static elem_t resadd2_buf[seq_len][hidden_dim];\
     \
@@ -246,11 +285,17 @@ uint64_t encoder_decoder(
             input, enc_out, output, \
             Wqkvo[0], Wqkvo[1], Wqkvo[2], Wqkvo[3],\
             Wqkvo_cross[0], Wqkvo_cross[1], Wqkvo_cross[2], Wqkvo_cross[3],\
+            \
+            Wqkvo_b[0], Wqkvo_b[1], Wqkvo_b[2], \
+            Wqkvo_b[2], \
+            Wqkvo_cross_b[0], Wqkvo_cross_b[1], Wqkvo_cross_b[2], \
+            Wqkvo_cross_b[3], \
+            \
             ff_w[0], ff_w[1], \
             ff1_b, ff2_b, \
             \
             QKV_buf[0], QKV_buf[1], QKV_buf[2], \
-            attn_buf, out_buf, \
+            attn_buf, out_buf, out_buf_acc, \
             resadd1_buf, resadd2_buf \
     ); \
     \
@@ -281,11 +326,11 @@ int main (int argc, char * argv[]) {
 
     gemmini_flush(0);
 
-    PRINT_ENCODER_DECODER("transformer-small", /*is_encoder=*/true,
-            /*hidden_dim=*/512, /*expansion_dim=*/1024, /*num_heads=*/4, /*cross_num_heads=*/4, /*seq_len=*/128, /*compression_factor=*/1);
-
     PRINT_ENCODER_DECODER("bert-base", /*is_encoder=*/true,
             /*hidden_dim=*/768, /*expansion_dim=*/3072, /*num_heads=*/12, /*cross_num_heads=*/12, /*seq_len=*/128, /*compression_factor=*/1);
+
+    PRINT_ENCODER_DECODER("transformer-small", /*is_encoder=*/true,
+            /*hidden_dim=*/512, /*expansion_dim=*/1024, /*num_heads=*/4, /*cross_num_heads=*/4, /*seq_len=*/128, /*compression_factor=*/1);
 
     exit(0);
 }
