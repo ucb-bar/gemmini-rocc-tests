4c7b34c1b7679e34aa5e4461108a59c34f21eb1c
Seah Kim <kimsea8209@a13.Millennium.Berkeley.EDU>
2023-09-25 14:00:58 -0700

    update

--- scratchpad/memcpy_dma.c
+++ scratchpad/memcpy_dma.c
@@ -19,11 +19,11 @@
 #define MAT_DIM_J 512
 
 #else
-#define MAT_DIM_I 32
-#define MAT_DIM_J 32
+#define MAT_DIM_I 4
+#define MAT_DIM_J 48
 #endif
 
-#define MAT_DIM 32
+//#define MAT_DIM 32
 #define NUM_ROW_TILE 2
 #define NUM_COL_TILE 2
 
@@ -31,6 +31,8 @@
 #define B_SCALE MVIN_SCALE_IDENTITY
 #define C_SCALE ACC_SCALE_IDENTITY
 #define USE_RELU true
+
+#define PageSize 4096
 
 void full_printMatrix(elem_t m[MAT_DIM_I][MAT_DIM_J]) {
   for (size_t i = 0; i < MAT_DIM_I; ++i) {
@@ -61,30 +63,36 @@ int main() {
     printf("I: %d, J: %d\n", MAT_DIM_I, MAT_DIM_J);
     int len = MAT_DIM_I * MAT_DIM_J;
 
-    gemmini_flush(0);
+    dma_flush(0);
 
-    static elem_t A[MAT_DIM_I][MAT_DIM_J] row_align(1) = {0};
-    static elem_t B[MAT_DIM_I][MAT_DIM_J] row_align(1) = {0};
-    static elem_t C[MAT_DIM_I][MAT_DIM_J] row_align(1) = {0};
-    static elem_t gold[MAT_DIM_I][MAT_DIM_J] row_align(1) = {0};
+    static elem_t A[MAT_DIM_I][MAT_DIM_J] row_align(MAX_BLOCK_LEN) = {0};
+    static elem_t B[MAT_DIM_I][MAT_DIM_J] row_align(MAX_BLOCK_LEN) = {0};
+    static elem_t C[MAT_DIM_I][MAT_DIM_J] row_align(MAX_BLOCK_LEN) = {0};
+    static elem_t gold[MAT_DIM_I][MAT_DIM_J] row_align(MAX_BLOCK_LEN) = {0};
 
-    uint64_t A_copy_addr = BASE_ADDR;
+    uint64_t A_copy_addr = (BASE_ADDR & ~(PageSize-1)) ;
+    printf("A copy addr: 0x%08lx\n", A_copy_addr);
+    printf("A source addr: 0x%08lx\n", A);
+
     printf("Init A and B\n");
     for (size_t i = 0; i < MAT_DIM_I; ++i) {
       for (size_t j = 0; j < MAT_DIM_J; ++j) {
         A[i][j] = (rand() % 64) - 32;
-        B[i][j] = (rand() % 8) - 4;
-        gold[i][j] = A[i][j]+B[i][j];
+        //B[i][j] = (rand() % 8) - 4;
+        //gold[i][j] = A[i][j]+B[i][j];
       }
     }
     printf("configuring DMA\n");
-    dma_config(channel, LOAD, (elem_t*) A, 0, MAT_DIM_J, MAT_DIM_J);
+    dma_source_config(channel, (elem_t*) A, MAT_DIM_J);
+    dma_dest_config(channel, A_copy_addr, MAT_DIM_J);
 
     printf("perform memcpy\n");
-    dma_memcpy_matrix(channel, 0, NUM_ROW_TILE, NUM_COL_TILE, MAT_DIM_I, MAT_DIM_J * sizeof(elem_t), MAT_DIM_I / NUM_ROW_TILE, (MAT_DIM_J*sizeof(elem_t))/NUM_COL_TILE);
+    bool granted = false;
+    int index = 0;
+    dma_memcpy_tile(channel, granted, 0, 0, index, MAT_DIM_I, (MAT_DIM_J) * sizeof(elem_t));
     
     //tiled_resadd_auto(MAT_DIM_I, MAT_DIM_J, 1, 1, 1, (elem_t*) A_copy_addr, (elem_t*) B, (elem_t*) C, false, WS);
-    printf("check result\n");
+    printf("granted %d, check result\n", granted);
     vec_is_equal(&A[0][0], (elem_t*) A_copy_addr, sizeof(A)/sizeof(elem_t));
     //vec_is_equal(&gold[0][0], &C[0][0], sizeof(gold)/sizeof(elem_t));
 
--- scratchpad/memcpy_dma_multi.c
+++ scratchpad/memcpy_dma_multi.c
@@ -9,6 +9,7 @@
 #include <sys/mman.h>
 #endif
 #include "include/gemmini_testutils.h"
+#include "include/dma.h"
 
 #define CHECK_RESULT 0 // 1
 
@@ -18,17 +19,23 @@
 #define MAT_DIM_J 512
 
 #else
-#define MAT_DIM_I 35
-#define MAT_DIM_J 27
+#define MAT_DIM_I 16 //1
+#define MAT_DIM_J 48 //20
 #endif
 
-#define BASE 0x70000000
-#define OFFSET 0
+//#define MAT_DIM 32
+#define NUM_ROW_TILE 2
+
+#define TILE_ROWS (MAT_DIM_I / NUM_ROW_TILE)
+#define TILE_COLS (MAT_DIM_J)
+
 
 #define A_SCALE 1
 #define B_SCALE MVIN_SCALE_IDENTITY
 #define C_SCALE ACC_SCALE_IDENTITY
 #define USE_RELU true
+
+#define PageSize 4096
 
 void full_printMatrix(elem_t m[MAT_DIM_I][MAT_DIM_J]) {
   for (size_t i = 0; i < MAT_DIM_I; ++i) {
@@ -53,31 +60,48 @@ int main() {
       exit(1);
     }
 #endif
+
+    int channel = 1;
+    printf("DMA channel: %d\n", channel);
     printf("I: %d, J: %d\n", MAT_DIM_I, MAT_DIM_J);
     int len = MAT_DIM_I * MAT_DIM_J;
 
-    //gemmini_flush(0);
+    dma_flush(0);
 
-    static elem_t A[MAT_DIM_I][MAT_DIM_J] row_align(1);
-    static elem_t B[MAT_DIM_I][MAT_DIM_J] row_align(1);
-    static elem_t C[MAT_DIM_I][MAT_DIM_J] row_align(1);
-    static elem_t gold[MAT_DIM_I][MAT_DIM_J];
+    static elem_t A[MAT_DIM_I][MAT_DIM_J] row_align(MAX_BLOCK_LEN) = {0};
+    static elem_t B[MAT_DIM_I][MAT_DIM_J] row_align(MAX_BLOCK_LEN) = {0};
+    static elem_t C[MAT_DIM_I][MAT_DIM_J] row_align(MAX_BLOCK_LEN) = {0};
+    static elem_t gold[MAT_DIM_I][MAT_DIM_J] row_align(MAX_BLOCK_LEN) = {0};
 
-    uint64_t A_copy_addr = BASE + OFFSET;
-    //elem_t* A_copy_addr = BASE + OFFSET;
-    // printf("Init A and B\n");
+    uint64_t A_copy_addr = (BASE_ADDR & ~(PageSize-1)) ;
+    printf("A copy addr: 0x%08lx\n", A_copy_addr);
+    printf("A source addr: 0x%08lx\n", A);
+
+    printf("Init A and B\n");
     for (size_t i = 0; i < MAT_DIM_I; ++i) {
       for (size_t j = 0; j < MAT_DIM_J; ++j) {
         A[i][j] = (rand() % 64) - 32;
-        B[i][j] = (rand() % 8) - 4;
-        gold[i][j] = A[i][j]+B[i][j];
+        //B[i][j] = (rand() % 8) - 4;
+        //gold[i][j] = A[i][j]+B[i][j];
       }
     }
-    memcpy((elem_t*) A_copy_addr, (elem_t*) A, sizeof(elem_t)*len);
+    printf("configuring DMA\n");
+    dma_source_config(channel, (elem_t*) A, MAT_DIM_J);
+    dma_dest_config(channel, A_copy_addr, MAT_DIM_J);
+
+    printf("perform memcpy\n");
+    bool granted = false;
+    int index = 0;
+    for (int tile = 0; tile < NUM_ROW_TILE; tile ++){
+        uint64_t source_addr_offset = tile * TILE_ROWS * MAT_DIM_J * sizeof(elem_t);
+        uint64_t dest_addr_offset = tile * TILE_ROWS * MAT_DIM_J * sizeof(elem_t);
+        dma_memcpy_multitile(channel, granted, source_addr_offset, dest_addr_offset, index, source_addr_offset, dest_addr_offset, 2, TILE_ROWS, (TILE_COLS) * sizeof(elem_t));
+    }
     //tiled_resadd_auto(MAT_DIM_I, MAT_DIM_J, 1, 1, 1, (elem_t*) A_copy_addr, (elem_t*) B, (elem_t*) C, false, WS);
+    printf("granted %d, check result\n", granted);
     vec_is_equal(&A[0][0], (elem_t*) A_copy_addr, sizeof(A)/sizeof(elem_t));
     //vec_is_equal(&gold[0][0], &C[0][0], sizeof(gold)/sizeof(elem_t));
-    printf("done\n");
+
     exit(0);
 }
 
